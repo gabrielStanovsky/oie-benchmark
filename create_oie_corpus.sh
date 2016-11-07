da19cafd41bec9fae5378ff1d402b43fe39f311b
gabis <gabriel.satanovsky@gmail.com>
2016-11-07 14:10:16 +0200

    split raw sentences to train/dev/test

--- create_oie_corpus.sh
+++ create_oie_corpus.sh
@@ -3,18 +3,18 @@ mkdir -p ./oie_corpus/newswire
 mkdir -p ./oie_corpus/wiki
 echo "Extracting from newswire..."
 echo "dev..."
-python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.dev.qa --out ./oie_corpus/newswire/propbank.dev.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.dev.qa --out ./oie_corpus/newswire/propbank.dev.oie --oieinput=./oie_corpus/newswire/propbank.dev.txt
 echo "train..."
-python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.train.qa --out ./oie_corpus/newswire/propbank.train.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.train.qa --out ./oie_corpus/newswire/propbank.train.oie --oieinput=./oie_corpus/newswire/propbank.train.txt
 echo "test..."
-python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.test.qa --out ./oie_corpus/newswire/propbank.test.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/newswire/propbank.test.qa --out ./oie_corpus/newswire/propbank.test.oie --oieinput=./oie_corpus/newswire/propbank.test.txt
 echo "Extracting from wiki..."
 echo "dev..."
-python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.dev.qa --out ./oie_corpus/wiki/wiki1.dev.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.dev.qa --out ./oie_corpus/wiki/wiki1.dev.oie --oieinput=./oie_corpus/wiki/wiki1.dev.txt
 echo "train..."
-python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.train.qa --out ./oie_corpus/wiki/wiki1.train.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.train.qa --out ./oie_corpus/wiki/wiki1.train.oie --oieinput=./oie_corpus/wiki/wiki1.train.txt
 echo "test..."
-python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.test.qa --out ./oie_corpus/wiki/wiki1.test.oie --oieinput=oie_corpus/oie_input.txt
+python ./qa_to_oie.py --in ./QASRL-full/wiki/wiki1.test.qa --out ./oie_corpus/wiki/wiki1.test.oie --oieinput=./oie_corpus/wiki/wiki1.test.txt
 echo "Concatenating global train/dev/test"
 cat ./oie_corpus/newswire/propbank.dev.oie ./oie_corpus/wiki/wiki1.dev.oie > ./oie_corpus/dev.oie
 cat ./oie_corpus/newswire/propbank.train.oie ./oie_corpus/wiki/wiki1.train.oie > ./oie_corpus/train.oie
