075e0c9580832ca4be7d2db3e9f0bdb7ec30cdac
Ajay Patel <patel.ajay285@gmail.com>
2019-07-24 12:18:03 -0700

    Removing ReVerb

--- eval.sh
+++ eval.sh
@@ -2,7 +2,6 @@
 mkdir -p ./eval/
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/Stanford.dat --stanford=./systems_output/stanford_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/OLLIE.dat --ollie=./systems_output/ollie_output.txt
-python benchmark.py --gold=./oie_corpus/all.oie --out=eval/ReVerb.dat --reverb=./systems_output/reverb_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/ClausIE.dat --clausie=./systems_output/clausie_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/OpenIE-4.dat --openiefour=./systems_output/openie4_output.txt
 python benchmark.py --gold=./oie_corpus/all.oie --out=eval/PropS.dat --props=./systems_output/props_output.txt
